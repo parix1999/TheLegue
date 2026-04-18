0646b997bfa3dfd8d3360b7bce592da96bc9a4f0
parix1999 <parisottoedoardo@gmail.com>
2026-04-18 19:22:53 +0200

    Fix NavBar e About page

--- index.js
+++ index.js
@@ -65,6 +65,10 @@ app.post("/comp", async (req, res) => {
 
 });
 
+app.get("/about", (req, res) => {
+  res.render("about.ejs");
+});
+
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
 });
