28eb691bbe0604bcf830ecce41ac960457d78a7b
parix1999 <parisottoedoardo@gmail.com>
2026-08-02 17:03:12 +0200

    aggiunta cambio stagione e bottone per la lista squadre

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ const port = 3000;
 const AuthToken = '79a282c9983b455bb083c96a8b3582f7';
 
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true })); // Middleware dati
 
 app.get("/", async (req, res) => {
   const options = {
@@ -31,16 +31,32 @@ app.get("/", async (req, res) => {
     console.log(error.message);
     res.status(500).send("Errore nel recupero dati");
   }
-
-  // res.render("index.ejs");
 });
 
 app.post("/comp", async (req, res) => {
+  const transcodeSeason = [
+    {
+      year: "2026",
+      code: "2026/27"
+    },
+    {
+      year: "2025",
+      code: "2025/26"
+    },
+    {
+      year: "2024",
+      code: "2024/25"
+    },
+    {
+      year: "2023",
+      code: "2023/24"
+    },
+  ];
   const data = JSON.parse(req.body.competition);
-  let year = data.startDate[0] + data.startDate[1] + data.startDate[2] + data.startDate[3]
+  var seasonYear = JSON.parse(req.body.sesaons);
 
   if (data.code === "CL" || data.code === "EC") {
-    year = null;
+    seasonYear = null;
   }
 
   const options = {
@@ -51,18 +67,23 @@ app.post("/comp", async (req, res) => {
         "Content-Type": "application/json",
         "X-Auth-Token": AuthToken,
       },
-      params: { season: year },
+      params: { season: seasonYear },
     },
   };
 
+  const result = transcodeSeason.find(({ year }) => year === String(seasonYear));
+
   try {
     const response = await axios.get(options.url + data.code + "/standings", options.config);
-    res.render("standing.ejs", { standHeader: response.data, standRow: response.data.standings[0] });
+    res.render("standing.ejs", {
+      standHeader: response.data,
+      standRow: response.data.standings[0],
+      seasonSelected: result.code
+    });
   } catch (error) {
     console.log(error.message);
     res.status(500).send("Errore nel recupero dati");
   }
-
 });
 
 app.get("/about", (req, res) => {
@@ -78,10 +99,10 @@ app.post("/statdata", async (req, res) => {
         "Content-Type": "application/json",
         "X-Auth-Token": AuthToken,
       },
-      params: { 
+      params: {
         season: req.body.year,
         competitions: req.body.code,
-        limit: 100 
+        limit: 100
       },
     },
   };
@@ -94,6 +115,11 @@ app.post("/statdata", async (req, res) => {
     res.status(500).send("Errore nel recupero dati");
   }
 
+});
+
+app.post("/teams", (req, res) => {
+  console.log(req.body);
+  res.render("teams.ejs");
 });
 
 app.listen(port, () => {
